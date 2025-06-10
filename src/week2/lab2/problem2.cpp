d1cd0371de530d8a9be7218cac50d1fb1f0493f1
akmal1216 <a.toshbotirov@newuu.uz>
2025-06-10 18:43:08 +0500

    lab4

---
+++ src/week2/lab2/problem2.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Toshbotirov Akmal on 10/06/25.
+//
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int grade;
+//     cout<<"Please input your grade: ";
+//     cin>>grade;
+//
+//     switch (grade/10) {
+//         case 10: cout<<'A';
+//         break;
+//         case 9: cout<<'A';
+//         break;
+//         case 8: cout<<'B';
+//         break;
+//         case 7: cout<<'C';
+//         break;
+//         case 6: cout<<'D';
+//         break;
+//         default: cout<<"F"<<endl;
+//     }
+//     return 0;
+// }
---
+++ src/week2/lab2/problem5.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Toshbotirov Akmal on 10/06/25.
+//
+// #include <iostream>
+// using namespace std;
+//
+// int main() {
+//     int number;
+//     char guess;
+//     number = 7;
+//     cout << "Guess whether the number is even or odd (e/o): ";
+//     cin >> guess;
+//     bool isEven = (number % 2 == 0);
+//     cout << "The number was: " << number << endl;
+//     if ((guess == 'e' && isEven) || (guess == 'o' && !isEven)) {
+//         cout << "Correct guess!" << endl;
+//     } else {
+//         cout << "Incorrect guess." << endl;
+//     }
+//
+//     return 0;
+// }
