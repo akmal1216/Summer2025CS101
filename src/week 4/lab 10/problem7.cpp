c7999bb5561398f252208403258c880de8f23690
a.toshbotirov <a.toshbotirov@newuu.uz>
2025-06-24 16:26:13 +0500

    lab10

--- src/week 4/lab 10/problem7.cpp
+++ src/week 4/lab 10/problem7.cpp
@@ -1,3 +1,30 @@
 //
 // Created by Akmaljon Toshbotirov on 23/06/25.
 //
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+//
+// int main() {
+//     int n, x;
+//     cin >> n;
+//     vector<int> v;
+//     for (int i = 0; i < n; i++) {
+//         cin >> x;
+//         if (x > 0) v.push_back(x);
+//     }
+//
+//
+//     for (int i = 0; i < v.size(); i++)
+//         for (int j = i + 1; j < v.size(); j++)
+//             if (v[j] < v[i]) {
+//                 int temp = v[i];
+//                 v[i] = v[j];
+//                 v[j] = temp;
+//             }
+//
+//     for (int i = 0; i < v.size(); i++) cout << v[i] << " ";
+//     cout << endl;
+//     return 0;
+// }
+
